b41fc8df6b04492f1e2ee88a0b84a3ce4e5d2fe5
kkporto <kakamportoo@gmail.com>
2025-06-03 17:15:18 -0400

    commit n 2 lab 4

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -5,27 +5,32 @@ import AllLabs from "./allLabs";
 import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
 
+import store from "./store";
+import { Provider } from "react-redux";
 
 export default function Labs() {
   return (
-    <div id="wd-labs">
-      <h1>Lab 1 - Home</h1>
-
-
-      <TOC />
-      <Routes>
-        <Route path="/" element={<Navigate to="allLabs" />} />
-        
-        <Route path="allLabs" element={<AllLabs />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2/*" element={<Lab2 />} />
-        <Route path="Lab3/*" element={<Lab3 />} />
-      </Routes>
-
-
     
+    <Provider store={store}>
+      <div id="wd-labs">
+        <h1>Lab 1 - Home</h1>
 
 
-    </div>
+        <TOC />
+        <Routes>
+          <Route path="/" element={<Navigate to="allLabs" />} />
+          
+          <Route path="allLabs" element={<AllLabs />} />
+          <Route path="Lab1" element={<Lab1 />} />
+          <Route path="Lab2/*" element={<Lab2 />} />
+          <Route path="Lab3/*" element={<Lab3 />} />
+          <Route path="Lab4/*" element={<Lab4 />} />
+        </Routes>
+
+
+
+      </div>
+    </Provider>
 );}
