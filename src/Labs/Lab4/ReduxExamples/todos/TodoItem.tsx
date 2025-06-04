0109f8bcc9eebb27e2fe03373a59dd76d4d7f45a
kkporto <kakamportoo@gmail.com>
2025-06-04 13:32:58 -0400

    lab 4, commit new, fixed bug n4 on to do

--- src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
+++ src/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
@@ -4,11 +4,8 @@ import { useDispatch } from "react-redux";
 import { deleteTodo, setTodo } from "./todosReducer";
 
 
-export default function TodoItem({ todo
-    // deleteTodo, setTodo }: {
-    // todo: { id: string; title: string };
-    // deleteTodo: (id: string) => void;
-    // setTodo: (todo: { id: string; title: string }) => void;
+export default function TodoItem({ todo }: {
+  todo: { id: string; title: string };
 }) {
 //   const { todo } = useSelector((state: any) => state.todosReducer);  
   const dispatch = useDispatch();
