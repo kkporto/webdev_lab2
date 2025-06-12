64efbee4dfd68b95a4db4f56573ea2211c7b5d49
kkporto <kakamportoo@gmail.com>
2025-06-12 11:10:11 -0400

    Fixed path bug commit

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -6,7 +6,8 @@ import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
 import Lab4 from "./Lab4";
-// import Lab5 from "./Lab5";
+import Lab5 from "./Lab5";
+// import { Lab5client } from "./Lab5";
 
 import store from "./store";
 import { Provider } from "react-redux";
@@ -28,7 +29,7 @@ export default function Labs() {
           <Route path="Lab2/*" element={<Lab2 />} />
           <Route path="Lab3/*" element={<Lab3 />} />
           <Route path="Lab4/*" element={<Lab4 />} />
-          {/* <Route path="Lab5/*" element={<Lab5 />} /> */}
+          <Route path="Lab5/*" element={<Lab5 />} />
         </Routes>
 
 
