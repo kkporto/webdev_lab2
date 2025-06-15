ea8959899550691617b57981b5350408130cf022
kkporto <kakamportoo@gmail.com>
2025-06-14 23:10:55 -0400

    fixing issue w status not used 2

--- src/Kambaz/Courses/Modules/index.tsx
+++ src/Kambaz/Courses/Modules/index.tsx
@@ -47,7 +47,11 @@ export default function Modules() {
   const { modules } = useSelector((state: any) => state.modulesReducer);
   const dispatch = useDispatch();
 
-  
+    const saveModule = async (module: any) => {
+    await modulesClient.updateModule(module);
+    dispatch(updateModule(module));
+  };
+
 
   const removeModule = async (moduleId: string) => {
     await modulesClient.deleteModule(moduleId);
@@ -105,7 +109,9 @@ export default function Modules() {
                onChange={(e) => dispatch(updateModule({ ...module, name: e.target.value }))}
                onKeyDown={(e) => {
                  if (e.key === "Enter") {
-                   dispatch(updateModule({ ...module, editing: false }));
+                  
+                     saveModule({ ...module, editing: false });
+                  //  dispatch(updateModule({ ...module, editing: false }));
                  }
                }}
                defaultValue={module.name}/>
