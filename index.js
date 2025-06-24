2900f61544871a44073df87ca5b9f5efb140c1b6
kkporto <kakamportoo@gmail.com>
2025-06-24 14:48:28 -0400

    working entire 5.3.5 update module, issue with axios, att 6

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ import Lab5 from './Lab5/index.js'
 import cors from "cors"
 
 import UserRoutes from "./Kambaz/Users/routes.js";
-
 import CourseRoutes from "./Kambaz/Courses/routes.js";
-
 import ModuleRoutes from "./Kambaz/Modules/routes.js";
 
 const app = express()
@@ -21,17 +19,24 @@ app.use(
     origin: process.env.NETLIFY_URL || "http://localhost:5173",
   })
 );
+
 const sessionOptions = {
   secret: process.env.SESSION_SECRET || "kambaz",
   resave: false,
   saveUninitialized: false,
+  cookie: {
+    secure: true,           
+    sameSite: "none",   }
+
 };
+
+
 if (process.env.NODE_ENV !== "development") {
   sessionOptions.proxy = true;
   sessionOptions.cookie = {
     sameSite: "none",
     secure: true,
-    domain: process.env.NODE_SERVER_DOMAIN,
+    // domain: process.env.NODE_SERVER_DOMAIN,
   };
 }
 app.use(session(sessionOptions));
