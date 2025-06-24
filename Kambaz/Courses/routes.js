b60385fb10f6cce6706543b4fdf20f8fc14e0a8b
kkporto <kakamportoo@gmail.com>
2025-06-24 11:36:33 -0400

    working up to 5.3.4.3, issue with loading initial cards

--- Kambaz/Courses/routes.js
+++ Kambaz/Courses/routes.js
@@ -25,6 +25,16 @@ export default function CourseRoutes(app) {
   app.get("/api/users/:userId/courses", findCoursesForEnrolledUser);
 
 
+  app.delete("/api/courses/:courseId", (req, res) => {
+    const { courseId } = req.params;
+    const status = dao.deleteCourse(courseId);
+    res.send(status);
+  });
+
+
+
+
+
 }
 
 
